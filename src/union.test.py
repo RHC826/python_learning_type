6d55a148e8f65ed7053a5545b2983ca60f8db74b
RHC826 <RHC826+RHC@users.noreply.github.com>
2024-11-13 03:50:18 +0900

    test のスケルトンを追加

--- src/union.test.py
+++ src/union.test.py
@@ -1,9 +1,15 @@
 import unittest
+import union
 
 
 class TestUnionMethod(unittest.TestCase):
+    """union.py unittest"""
+
     def test_union(self):
-        self.assertTrue(True,"test")
+        self.assertTrue(True, "test")
+
+    def test_main(self):
+        self.assertEqual(union.main(), True)
 
 
 if __name__ == "__main__":
