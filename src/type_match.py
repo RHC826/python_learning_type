67c6843c9414921c29a5f1fb2ab24a5d5a7bb93b
RHC <RHC826+RHC@users.noreply.github.com>
2024-11-24 01:58:35 +0900

    add met.py

--- src/type_match.py
+++ src/type_match.py
@@ -1,11 +1,24 @@
 """match 文で型分岐"""
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Card:
+    """トランプクラス"""
+
+    suit: str
+    number: int
+
 
 def main(arg):
     """分岐"""
     match arg:
+        case {"suit":x,"number":y} :
+            print("x,y:",x,y)
+            return f"Card dict! Suit:{x} Number:{y}"
         case str():
-            print("Strin!")
+            print("String!")
             return "String!"
         case int():
             print("Int!")
@@ -19,10 +32,41 @@ def main(arg):
         case dict():
             print("Dict!")
             return ""
+        # dataclass の分岐
+        case Card(suit, number):
+            print("Card class!")
+            return (suit, number)
         case _:
-            return ""
-    raise LookupError("error!")
+            return "UNKNOWN"
+    raise KeyError("error!")
+
+# """ポーランド記法の計算"""
+type Pn = tuple[str, Pn|int|float, Pn|int|float]
+def pn_calculator(expression: Pn|int|float) -> int | float:
+    """ポーランド記法の式を計算する"""
+    match expression:
+        case ("+", left, right):
+            return pn_calculator(left) + pn_calculator(right)
+        case ("-", left, right):
+            return pn_calculator(left) - pn_calculator(right)
+        case ("*", left, right):
+            return pn_calculator(left) * pn_calculator(right)
+        case ("/", left, right):
+            return pn_calculator(left) / pn_calculator(right)
+        case int() | float():
+            return expression
+    raise ValueError("無効な式です")
 
 
 if __name__ == "__main__":
-    pass
+    D5 = Card("ダイヤ", 5)
+    print(main(D5))
+    DDD = {
+        "suit":"ダイヤ",
+        "number": 5
+    }
+    print(main(DDD))
+    print(type(D5))
+    print(pn_calculator(("+", 2, 3)))
+    print(pn_calculator(("+", ("+",1,1), 3)))
+    print(pn_calculator(("+", ("/",5.0,2), 3)))
