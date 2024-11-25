e1b45cd34baaede6f2cac423b59c60dd4caa69b6
RHC <RHC826+RHC@users.noreply.github.com>
2024-11-26 00:57:56 +0900

    add sphinx

--- src/met.py
+++ src/met.py
@@ -9,26 +9,23 @@ def main(query: str = "sunflowers") -> None:
     """1. キーワード検索 2. 該当する作品の情報を得る
     Document URL:https://metmuseum.github.io/
     """
+    res = []
+    total = 0
     domain = "https://collectionapi.metmuseum.org"
 
     request = requests.get(
         f"{domain}/public/collection/v1/search?q={query}&=hasImages=true", timeout=10
     )
 
-    print(request)
     match request.status_code:
         case 200:
             match json.loads(request.text):
                 case {"total": total, "objectIDs": ids}:
-                    print(":::", total)
                     if total == 0:
                         return
 
                     for cnt, _id in enumerate(ids):
                         request_work_query = f"/public/collection/v1/objects/{_id}"
-                        print(
-                            f"https://www.metmuseum.org/art/collection/search/{_id}"
-                        )
 
                         work = requests.get(
                             f"{domain}/{request_work_query}", timeout=10
@@ -46,6 +43,10 @@ def main(query: str = "sunflowers") -> None:
                                     case {"title": title, "primaryImage": url} if len(
                                         url
                                     ) > 0 and str(url):
+                                        res.append(work)
+                                        print(
+                                            f"https://www.metmuseum.org/art/collection/search/{_id}"
+                                        )
                                         print(
                                             f"""
     {title}
@@ -56,10 +57,26 @@ def main(query: str = "sunflowers") -> None:
                                         )
                         # リクエスト制限
                         time.sleep(0.1)
-                        if cnt > 10:
+                        if cnt > 100:
                             break
+    print(res)
+    print(f"total:{total}")
 
-    print("now working")
+def sain(query: str = "sunflowers") -> None:
+    """1. キーワード検索 2. 該当する作品の情報を得る
+    Document URL:https://metmuseum.github.io/
+    """
+    domain = "https://collectionapi.metmuseum.org"
 
+    request = requests.get(
+        f"{domain}/public/collection/v1/search?q={query}&=hasImages=true", timeout=10
+    )
+
+    match request.status_code:
+        case 200:
+            match json.loads(request.text):
+                case {"total": total, "objectIDs": ids} if total > 0:
+                    for id in ids:
+                        print(f"https://www.metmuseum.org/art/collection/search/{id}")
 
 main()
